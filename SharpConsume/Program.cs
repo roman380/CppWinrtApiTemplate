dd473cb6a7c2514f236a8439cd90e3ede997d95d
Roman Ryltsov <roman@alax.info>
2020-03-15 16:38:31 +0200

    Added .NET Framework consume API project

---
+++ SharpConsume/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Api;
+
+// NOTE: Add references to 
+// - C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.18362.0\Windows.winmd
+// - Api.winmd
+// and see app.manifest for Api.dll reference
+
+namespace SharpConsume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            Console.WriteLine($"{myClass.Text}");
+        }
+    }
+}
